98e0c9f4e97a3aad337d66ed57a85f187769511c
IceKhoffi <war45949@gmail.com>
2025-11-22 21:23:17 +0700

    Initial Commit

---
+++ utils/load_db.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+load_dotenv()
+
+CONN_STRING = f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DATABASE')}"
+
+def get_conn_string():
+    return CONN_STRING
+
+def get_mysql_conn():
+    return mysql.connector.connect(
+        host=os.getenv('MYSQL_HOST'),
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        database=os.getenv('MYSQL_DATABASE'),
+        port=os.getenv('MYSQL_PORT')
+    )
